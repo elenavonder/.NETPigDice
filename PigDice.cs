012c73746ac72f77637c666374431687e5c3f68e
elenavonder <evonder13@gmail.com>
2021-02-09 16:19:36 -0500

    added method

--- PigDice.cs
+++ PigDice.cs
@@ -3,6 +3,11 @@ namespace PigDiceGame
 {
     public class PigDice
     {
+        public int Play() {
+            var score = 0;
+            var roll = roll();
+
+        }
         public PigDice()
         {
         }
